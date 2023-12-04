96663740db58f551d4422e3d8b5f3fa0d2ff0032
nesterov-n <nikolaynesterov@gmail.com>
2023-12-04 21:58:46 +0400

    Fix crash in task order setup

--- plugin/src/main/kotlin/ru/cian/rustore/publish/RustorePublishPlugin.kt
+++ plugin/src/main/kotlin/ru/cian/rustore/publish/RustorePublishPlugin.kt
@@ -14,8 +14,6 @@ import org.gradle.kotlin.dsl.withType
 
 class RustorePublishPlugin : Plugin<Project> {
 
-    private val tasksMustRunAfter = mutableMapOf<String, String>()
-
     override fun apply(project: Project) {
         project.plugins.withType<AppPlugin> {
             configureRustorePublish(project)
@@ -30,59 +28,25 @@ class RustorePublishPlugin : Plugin<Project> {
 
         val androidComponents = project.extensions.getByType<ApplicationAndroidComponentsExtension>()
         androidComponents.onVariants(androidComponents.selector().all() as VariantSelector) { variant ->
-            createTask(project, variant, rustorePublishExtension)
+            createTask(project, variant)
         }
     }
 
     @Suppress("DefaultLocale")
     private fun createTask(
         project: Project,
-        variant: ApplicationVariant,
-        rustorePublishExtension: RustorePublishExtension,
+        variant: ApplicationVariant
     ) {
         val variantName = variant.name.capitalize()
         val publishTaskName = "${RustorePublishTask.TASK_NAME}$variantName"
-        val publishTask = project.tasks.register<RustorePublishTask>(publishTaskName, variant)
-        val extension = rustorePublishExtension.instances.find { it.name.equals(variant.name, ignoreCase = true) }
-        val applyConfigureOptimization = extension?.pluginSettings?.applyConfigureOptimization
-        tasksMustRunAfter.put(publishTaskName, "assemble$variantName")
-        tasksMustRunAfter.put(publishTaskName, "bundle$variantName")
-
-        sheduleTasksOrder(applyConfigureOptimization, publishTask, project, variantName)
-    }
-
-    private fun sheduleTasksOrder(
-        applyConfigureOptimization: Boolean?,
-        publishTask: TaskProvider<RustorePublishTask>,
-        project: Project,
-        variantName: String
-    ) {
-        if (applyConfigureOptimization == true) {
-            publishTask.configure {
-    //            println("--> RustorePublishTask registered: ($publishTaskName)")
-    //            project.tasks.forEach {
-    //                println("--> (1): ${it.name}")
-    //            }
-    //                setMustRunAfter(
-    //                    setOf(
-    ////                        project.tasks.named("clean"),
-    //                        project.tasks.named("assemble$variantName"),
-    //                        project.tasks.named("bundle$variantName"),
-    //                    )
-    //                )
-                if (project.tasks.findByName("assemble$variantName") != null) {
-                    setMustRunAfter(setOf(project.tasks.named("assemble$variantName")))
-                }
-                if (project.tasks.findByName("bundle$variantName") != null) {
-                    setMustRunAfter(setOf(project.tasks.named("bundle$variantName")))
-                }
-            }
-        } else {
-            project.tasks.whenTaskAdded {
-                if (this.name == "assemble$variantName" || this.name == "bundle$variantName") {
-                    publishTask.get().mustRunAfter(this)
-                }
-            }
+        project.tasks.register<RustorePublishTask>(publishTaskName, variant).configure {
+            setMustRunAfter(
+                setOf(
+                    project.tasks.named("assemble$variantName"),
+                    project.tasks.named("bundle$variantName"),
+                )
+            )
         }
+
     }
 }
